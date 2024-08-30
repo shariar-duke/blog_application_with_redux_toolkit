c645769f4eeb2bb440f2373115010548feffd845
shariar-duke <shariarmahmud225@gmail.com>
2024-08-31 02:19:32 +0600

    data fetched in BlogGrid

--- src/components/grid/BlogGrid.jsx
+++ src/components/grid/BlogGrid.jsx
@@ -1,6 +1,19 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBlogs } from "../../features/blogs/blogsSlice";
+import Loading from "../ui/Loading";
 import BlogGridItem from "./BlogGridItem"
 export default function BlogGrid() {
+
+    const dispatch = useDispatch()
+
+    // blogs er state theke amader j j data lagbe 
+    const { blogs, isLoading, isError, error } = useSelector((state) => state.blogs)
+    useEffect(() => {
+        dispatch(fetchBlogs())
+    }, [dispatch])
+
+    console.log("The blogs items are", blogs)
     return (
         <div className="grid grid-cols-3 gap-[30px]">
 
