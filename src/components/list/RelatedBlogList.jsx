f04d8006b55b49c8b2e82768b7ac0e5202c07ceb
shariar-duke <shariarmahmud225@gmail.com>
2024-08-30 23:57:40 +0600

    Related BlogList component

---
+++ src/components/list/RelatedBlogList.jsx
@@ -0,0 +1,8 @@
+import RelatedBlogItem from "./RelatedBlogItem"
+export default function RelatedBlogList() {
+    return (
+        <div className="flex flex-col gap-[40px]">
+            <RelatedBlogItem />
+        </div>
+    )
+}
