86ccc3fc01b6162d97d69f2b370af778ed4ce501
shariar-duke <shariarmahmud225@gmail.com>
2024-08-31 03:19:19 +0600

    blogReducer added on store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogsReducer from "../features/blogs/blogsSlice"
+import blogReducer from "../features/blog/blogSlice"
 
 export const store = configureStore({
     reducer:{
-        blogs: blogsReducer
+        blogs: blogsReducer, 
+        blog:blogReducer
     }
 })
