54c9b39808b0f49ad1d43dab1af6a107068eed5b
shariar-duke <shariarmahmud225@gmail.com>
2024-08-31 00:05:43 +0600

    router added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:blogId" element={<Blog />} />
+      </Routes>
+    </Router>
   )
 }
