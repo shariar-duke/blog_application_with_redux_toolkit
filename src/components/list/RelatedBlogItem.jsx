cab420e4191230a9023b6575206040db16533e52
shariar-duke <shariarmahmud225@gmail.com>
2024-08-30 23:58:13 +0600

    RelatedBlogListItem compnent

---
+++ src/components/list/RelatedBlogItem.jsx
@@ -0,0 +1,13 @@
+
+export default function RelatedBlogItem() {
+    return (
+        <div className="rounded-md shadow-md">
+            <div style={{ backgroundImage: "url(/git.webp)" }} className="h-[160px] bg-cover bg-center"></div>
+            <div className="p-[14px]">
+                <p className="text-[18px] font-bold">Top Github Alternatives</p>
+                <p className="text-[16px] font-medium">#python, #tech, #git</p>
+                <p className="text-[16px] font-medium">2010-03-27</p>
+            </div>
+        </div>
+    )
+}
