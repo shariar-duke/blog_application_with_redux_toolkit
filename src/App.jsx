4cac5c0708b7cc0de4f26c0cf88f2d0527a00192
shariar-duke <shariarmahmud225@gmail.com>
2024-08-31 01:57:37 +0600

    store provided in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Home from "./pages/Home"
 import Blog from "./pages/Blog"
+import { Provider } from "react-redux";
+import { store } from "./app/store"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog/:blogId" element={<Blog />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:blogId" element={<Blog />} />
+        </Routes>
+      </Router>
+    </Provider>
+
   )
 }
