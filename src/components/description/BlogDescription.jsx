1ca701aaba8e82bc98d5fefd33e7003324f25fe3
shariar-duke <shariarmahmud225@gmail.com>
2024-08-31 07:54:33 +0600

    data added in Blog page

--- src/components/description/BlogDescription.jsx
+++ src/components/description/BlogDescription.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable react/prop-types */
 import { VscSave } from "react-icons/vsc";
 import { AiOutlineLike } from "react-icons/ai";
-export default function BlogDescription() {
+export default function BlogDescription({ blog }) {
+    const { tags, description, title } = blog
     return (
         <div className="mb-[20px]">
-            <p className="text-[24px] font-bold font-inter">MERN Stack For Web Development</p>
-            <p className="text-[18px] font-medium text-gray-600">#python, #tech, #git</p>
+            <p className="text-[24px] font-bold font-inter">{title}</p>
+            <p className="text-[16px]">
+                {tags && tags.map((tag) => `#${tag}`).join(', ')}
+            </p>
+
             <div className="flex gap-[16px] mt-[12px]">
                 <div className="flex items-center cursor-pointer">
                     <AiOutlineLike size={18} />
@@ -19,7 +24,7 @@ export default function BlogDescription() {
                 </div>
             </div>
 
-            <p className="text-[17px] mt-[6px] font-medium text-gray-400">A MERN stack comprises a collection of four frameworks (MongoDB, ExpressJs, ReactJs and NodeJs) used to develop full-stack javascript solutions for rapid, scalable, and secure applications. Each framework serves a different purpose in creating successful web applications. It is an excellent choice for companies looking to develop high-quality responsive applications quickly using just one language.</p>
+            <p className="text-[17px] mt-[6px] font-medium text-gray-400">{description}</p>
         </div>
     )
 }
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,24 +2,71 @@ import BlogHeroSection from "../components/description/BlogHeroSection"
 import Navbar from "../components/layout/Navbar"
 import BlogDescription from "../components/description/BlogDescription"
 import RelatedBlogList from "../components/list/RelatedBlogList"
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBlog } from "../features/blog/blogSlice";
+import { useEffect } from "react";
+import Loading from "../components/ui/Loading";
+import { useParams } from "react-router-dom";
 export default function Blog() {
+
+    const dispatch = useDispatch()
+    const { blog, isError, isLoading, error } = useSelector(
+        (state) => state.blog
+    );
+
+    // blg ta ber kore ante hbe but tar jnno of course id lagbe 
+
+    const { blogId } = useParams()
+
+    // now fetching the singleBlog . I need to fetch it inside the useEffect hook 
+
+    useEffect(() => {
+        dispatch(fetchBlog(blogId))
+    }, [blogId, dispatch])
+
+    const { image } = blog
+    // decide what to render 
+    let content = null;
+
+    // jode loading state a thake
+    if (isLoading) content = <Loading />;
+    // jode error thake
+    if (!isLoading && isError)
+        content = <div className="col-span-12">{error}</div>;
+
+    // jode response ase but blank ekta object aslo . blank object a to id thakbe na
+    if (!isLoading && !isError && !blog.id) {
+        content = <div className="col-span-12">No video found </div>;
+    }
+
+    if (!isLoading && !isError && blog.id) {
+        content = (
+
+            <div className="mt-[16px] h-[80vh] grid grid-cols-7 px-[84px] gap-[80px]">
+                <div className="col-span-5 grid grid-rows-6 gap-[30px]">
+                    <div className="row-span-4 border rounded-md bg-cover bg-center overflow-hidden" style={{ backgroundImage: `url(${image})` }}></div>
+                    <div className="row-span-2">
+                        <BlogDescription blog={blog} />
+                    </div>
+                </div>
+                <div className="col-span-2">
+                    <RelatedBlogList />
+                </div>
+            </div>)
+
+    }
+
     return (
         <div >
             <Navbar />
             <div>
                 <BlogHeroSection />
             </div>
-            <div className="mt-[16px] h-[80vh] grid grid-cols-7 px-[84px] gap-[80px]">
-                <div className="col-span-5 grid grid-rows-6 gap-[30px]">
-                    <div className="row-span-4 border rounded-md bg-cover bg-center overflow-hidden" style={{ backgroundImage: "url(/mern.webp)" }}></div>
-                    <div className="row-span-2">
-                        <BlogDescription />
-                    </div>
-                </div>
-                <div className="col-span-2">
-                    <RelatedBlogList />
-                </div>
-            </div>
+
+            {
+                content
+            }
+
         </div>
     )
 }
