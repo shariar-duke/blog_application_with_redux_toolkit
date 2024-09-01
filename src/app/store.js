64679bf7ffd45ef957b9a77ee706c5c370c74d81
shariar-duke <shariarmahmud225@gmail.com>
2024-09-01 15:48:07 +0600

    relatedBlogs added in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogsReducer from "../features/blogs/blogsSlice"
 import blogReducer from "../features/blog/blogSlice"
+import relatedBlogReducer from "../features/relatedBlogs/relatedBlogsSlice"
 
 export const store = configureStore({
     reducer:{
         blogs: blogsReducer, 
-        blog:blogReducer
+        blog:blogReducer,
+        relatedBlogs:relatedBlogReducer
     }
 })
