f5e795935b07d2a7f2fbcb9137a7388f9178e30a
shariar-duke <shariarmahmud225@gmail.com>
2024-09-01 15:59:43 +0600

    action dispatched from RelatedBlogList

--- src/components/list/RelatedBlogList.jsx
+++ src/components/list/RelatedBlogList.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable react/prop-types */
 import RelatedBlogItem from "./RelatedBlogItem"
-export default function RelatedBlogList() {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRelatedBlogs } from "../../features/relatedBlogs/relatedBlogsSlice";
+import Loading from "../ui/Loading";
+export default function RelatedBlogList({ tags, id }) {
+    const { relatedBlogs, isLoading, isError, error } = useSelector((state) => state.relatedBlogs)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchRelatedBlogs({ id: id, tags }))
+    }, [dispatch, tags, id])
+
+    // let's decide which content will render
+    let content;
+    if (isLoading) {
+        content = <Loading />;
+    }
+
+    if (!isLoading && isError) {
+        content = <div className="col-span-12">{error}</div>;
+    }
+
+    if (!isError && !isLoading && relatedBlogs?.length == 0) {
+        content = <div className="col-span-12">No Blogs Found</div>;
+    }
+    if (!isError && !isLoading && relatedBlogs?.length > 0) {
+        content = relatedBlogs.map((blog) => <RelatedBlogItem key={blog.id} blog={blog} />)
+    }
     return (
         <div className="flex flex-col gap-[40px]">
-            <RelatedBlogItem />
+            {content}
         </div>
     )
 }
